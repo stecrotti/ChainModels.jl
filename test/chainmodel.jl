beb58a1b7c035cb53cd0a4b960a3eb839dfa3d80
stecrotti <sky_96@live.it>
2025-11-17 16:39:03 +0100

    test random constructor

--- test/chainmodel.jl
+++ test/chainmodel.jl
@@ -1,3 +1,29 @@
+@testset "Constructor with fields" begin
+    h = [rand(qi) for qi in qs]
+    p = ChainModel(f, h)
+
+    function ev(f, h, x)
+        w = 0.0
+        for i in eachindex(f)
+            w += f[i][x[i],x[i+1]]
+        end
+        for i in eachindex(h)
+            w += h[i][x[i]]
+        end
+        return w
+    end
+
+    P = [logevaluate(p, x) for x in Iterators.product((1:q for q in qs)...)]
+    Ptest = [ev(f, h, x) for x in Iterators.product((1:q for q in qs)...)]
+    @test P ≈ Ptest
+end
+
+@testset "Random constructor" begin
+    p = rand_chain_model(10, 4)
+    @test length(p) == 10
+    @test all(isequal(4), nstates(p))
+end
+
 marg = @inferred marginals(chain)
 nmarg = @inferred neighbor_marginals(chain)
 pmarg = @inferred pair_marginals(chain)
@@ -34,24 +60,4 @@ end
     E = @inferred energy(chain)
     E_exhaust = sum(-logevaluate(chain, x)*p for (x,p) in pairs(P))
     @test E ≈ E_exhaust
-end
-
-@testset "Constructor with fields" begin
-    h = [rand(qi) for qi in qs]
-    p = ChainModel(f, h)
-
-    function ev(f, h, x)
-        w = 0.0
-        for i in eachindex(f)
-            w += f[i][x[i],x[i+1]]
-        end
-        for i in eachindex(h)
-            w += h[i][x[i]]
-        end
-        return w
-    end
-
-    P = [logevaluate(p, x) for x in Iterators.product((1:q for q in qs)...)]
-    Ptest = [ev(f, h, x) for x in Iterators.product((1:q for q in qs)...)]
-    @test P ≈ Ptest
 end
