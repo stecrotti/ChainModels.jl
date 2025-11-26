29689679fbc3c3fbc12cc4df1ddfac59ae213640
stecrotti <sky_96@live.it>
2025-11-26 10:58:29 +0100

    simplify interface for marginals, rename energy -> avg_energy

--- src/ChainModels.jl
+++ src/ChainModels.jl
@@ -13,7 +13,7 @@ export ChainModel, rand_chain_model, nstates,
         accumulate_left!, accumulate_right!, accumulate_left, accumulate_right,
         accumulate_middle, accumulate_middle!, logevaluate, evaluate, lognormalization,
         normalization, normalize!, normalize,
-        marginals, neighbor_marginals, pair_marginals, energy
+        marginals, neighbor_marginals, pair_marginals, avg_energy
         # overrides from Distributions, StatsBase
         rand, mean, var, cov, entropy,
         kldivergence, loglikelihood, logpdf, pdf
@@ -25,6 +25,7 @@ include("overrides.jl")
 include("k_chainmodel.jl")
 
 export KChainModel, rand_kchain_model, k_accumulate_left, k_accumulate_left!,
-    k_accumulate_right, k_accumulate_right!, Km1_neighbor_marginals
+    k_accumulate_right, k_accumulate_right!, Km1_neighbor_marginals,
+    nbody_neighbor_marginals
 
 end
